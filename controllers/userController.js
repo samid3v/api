619ef5a613c9c471f6cd50a315f990d1aa27e57e
samd3v <swanyoike154@gmail.com>
2024-02-09 00:14:10 +0300

    jwt token fix

--- controllers/userController.js
+++ controllers/userController.js
@@ -59,7 +59,11 @@ export const userLogin = asyncHandler(async (req, res) => {
 
           const token = jwt.sign({ username }, process.env.API_SECRET, { expiresIn: '24h' });
 
-          res.cookie('token', token, { httpOnly: true , domain: 'https://petfarm.onrender.com'})
+          res.cookie('token', token, { 
+            httpOnly: true , 
+            sameSite: 'none',
+            secure:true
+          })
           .status(200)
           .json({ message: 'Login successful',
             user,
