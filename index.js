d7771e14b1c194050c774c743e970b6b9c173a1b
samd3v <swanyoike154@gmail.com>
2024-02-09 21:53:50 +0300

    server update

--- index.js
+++ index.js
@@ -48,11 +48,11 @@ app.use("/api/analytics", analyticsRouter)
 
 app.use(errorHandler);
 
-app.use(express.static(path.join(__dirname, '/client/dist')))
+// app.use(express.static(path.join(__dirname, '/client/dist')))
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
-})
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+// })
 
 
 app.listen(port, ()=>{
